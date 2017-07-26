828d05bacca963fc74753d78e949c1478a0f72a7
Diego <zdr_00@live.com>
2017-07-26 02:41:24 +0000

    Scaffolded REST API and configured environment variables and database connection

---
+++ api.md
@@ -0,0 +1,65 @@
+### loan
+  - askedDate
+  - dueDate
+  - amount
+  - status
+  - toPay
+  - userEmail
+  - idUser
+
+### user
+  - hasLoan
+  - conekta_id
+  - conekta_card
+  - email
+  - kintos
+  - loansAmount
+  - name
+  - password
+  - picturePath
+  - school
+  - city
+  - referral_code
+  - no_referred
+
+### reward
+  - brief
+  - description
+  - logo
+  - name
+  - price
+  - restrictions
+  - requiredLevel
+
+### wallet
+  - brief
+  - decription
+  - logo
+  - name
+  - price
+  - type
+  - restrictions
+  - requiredLevel
+  - email
+ 
+### university
+  - nombre
+  - url
+  - launch day
+  - date added
+  - city
+  - state
+  - address
+  - phone
+
+### business
+  - name
+  - type
+  - city
+  - address
+  - state
+  - zip code
+  - phone number
+  - rfc
+  - logo
+  - url
---
+++ src/config.js
@@ -0,0 +1,70 @@
+/* eslint-disable no-unused-vars */
+import path from 'path'
+import _ from 'lodash'
+
+/* istanbul ignore next */
+const requireProcessEnv = (name) => {
+  if (!process.env[name]) {
+    throw new Error('You must set the ' + name + ' environment variable')
+  }
+  return process.env[name]
+}
+
+/* istanbul ignore next */
+if (process.env.NODE_ENV !== 'production') {
+  const dotenv = require('dotenv-safe')
+  dotenv.load({
+    path: path.join(__dirname, '../.env'),
+    sample: path.join(__dirname, '../.env.example')
+  })
+}
+
+const config = {
+  all: {
+    env: process.env.NODE_ENV || 'development',
+    root: path.join(__dirname, '..'),
+    port: process.env.PORT || 9000,
+    ip: process.env.IP || '0.0.0.0',
+    defaultEmail: 'no-reply@kintos-api.com',
+    sendgridKey: requireProcessEnv('SENDGRID_KEY'),
+    masterKey: requireProcessEnv('MASTER_KEY'),
+    jwtSecret: requireProcessEnv('JWT_SECRET'),
+    mongo: {
+      options: {
+        db: {
+          safe: true
+        }
+      }
+    }
+  },
+  test: {
+    mongo: {
+      uri: 'mongodb://nattiv.mx/test',
+      options: {
+        user: "kintos-admin",
+        pwd: "kintos-pwa",
+        debug: false
+      }
+    }
+  },
+  development: {
+    mongo: {
+      uri: 'mongodb://nattiv.mx/kintos-api-dev',
+      options: {
+        user: process.env.KINTOS_MONGODB_USER,
+        pwd: process.env.KINTOS_MONGODB_PWD,
+        debug: true
+      }
+    }
+  },
+  production: {
+    ip: process.env.IP || undefined,
+    port: process.env.PORT || 8080,
+    mongo: {
+      uri: process.env.MONGODB_URI || 'mongodb://nattiv.mx/kintos-api'
+    }
+  }
+}
+
+module.exports = _.merge(config.all, config[config.all.env])
+export default module.exports
